aa99c77806fae117db611ad26bf5c2fc3ef656b4
AmirFragman <amir.fragman@gmail.com>
2023-05-09 16:09:39 +0300

    deletion of false

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import tools.col as col
 from tools.numbers import simp
 from tools.numbers import comp
 
-HAS_CALLED_SIMP_FUNCTIONS = False
 if __name__ == "__main__":
     simp.sum_of_nums()
     simp.subtraction_of_nums()
